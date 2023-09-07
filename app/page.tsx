c32e580fa34c46280bf9da69d8dcca9f1c7bc7b9
Daniele Terracciano <daniele.terracciano@business-changers.it>
2023-09-07 17:22:57 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+//@ts-nocheck
+"use client"
+import { Dock } from "@/components/navbar/Dock";
+import { DockCard } from "@/components/navbar/DockCard";
+import { Card } from "@/components/navbar/Card";
+import { DockDivider } from "@/components/navbar/DockDivider";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { particlesOptions } from "@/components/Particles/particlesConfig";
+import { Engine } from "tsparticles-engine";
+import { NextPage } from "next";
+
+const Page : NextPage = () => {
+    
+    return (
+        <div>
+            
+            
+        </div>
+    )
+}
+
+export default Page;
